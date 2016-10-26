1d3f47389a6b0ed4c68262bfc3255a9ccfa9c3be
Aedan Smith <aedan.smith@gmail.com>
2016-10-26 11:21:04 -0700

    Improved function parser

--- src/nylon/parser/FunctionParser.java
+++ src/nylon/parser/FunctionParser.java
@@ -1,5 +1,6 @@
 package nylon.parser;
 
+import javafx.util.Pair;
 import nylon.NylonRuntime;
 import nylon.exceptions.NylonRuntimeException;
 import nylon.functions.*;
@@ -18,46 +19,59 @@ public final class FunctionParser {
     public static LinkedList<NylonFunction> parse(NylonRuntime runtime, String src) throws NylonRuntimeException {
         LinkedList<NylonFunction> functions = new LinkedList<>();
         for (int i = 0; i < src.length(); i++) {
-            if (Character.isDigit(src.charAt(i))) {
-                int j = i;
-                for (; i < src.length(); i++) {
-                    if (!Character.isDigit(src.charAt(i)))
-                        break;
-                }
-                functions.add(new PushNylonObjectFunction<>(new NylonLong(Long.parseLong(src.substring(j, i)))));
-                i--;
-                continue;
-            }
-            // If the character is [a-zA-Z]
-            if ((src.charAt(i) >= 97 && src.charAt(i) <= 122) || (src.charAt(i) >= 65 && src.charAt(i) <= 90)) {
-                int j = i;
-                for (; i < src.length(); i++) {
-                    if (!Character.isAlphabetic(src.charAt(i)))
-                        break;
-                }
-                if (i == j + 1)
-                    functions.add(new PushNylonObjectFunction<>(new NylonCharacter(src.charAt(j))));
-                else
-                    functions.add(new PushNylonObjectFunction<>(new NylonString(src.substring(j, i))));
-                i--;
-                continue;
-            }
-            if (src.charAt(i) == '[') {
-                int depth = 1, j = i + 1;
-                for (i = j; i < src.length(); i++) {
-                    if (src.charAt(i) == '[')
-                        depth++;
-                    if (src.charAt(i) == ']')
-                        depth--;
-                    if (depth == 0)
-                        break;
-                }
-                functions.add(new NylonSrcFunction(runtime, src.substring(j, i), -1));
-                continue;
-            }
-            functions.add(runtime.getFunctionDictionary().get(runtime, src.charAt(i)));
+            Pair<Integer, NylonFunction> pair = parseNextFunction(runtime, src, i);
+            functions.add(pair.getValue());
+            i = pair.getKey();
         }
         return functions;
+    }
+
+    private static Pair<Integer, NylonFunction> parseNextFunction(NylonRuntime runtime, String src, int i)
+            throws NylonRuntimeException {
+        if (Character.isDigit(src.charAt(i))) {
+            int j = i;
+            for (; i < src.length(); i++) {
+                if (!Character.isDigit(src.charAt(i)))
+                    break;
+            }
+            return new Pair<>(
+                    i-1, new PushNylonObjectFunction<>(new NylonLong(Long.parseLong(src.substring(j, i))))
+            );
+        }
+        // If the character is [a-zA-Z]
+        if ((src.charAt(i) >= 97 && src.charAt(i) <= 122) || (src.charAt(i) >= 65 && src.charAt(i) <= 90)) {
+            int j = i;
+            for (; i < src.length(); i++) {
+                if (!Character.isAlphabetic(src.charAt(i)))
+                    break;
+            }
+            if (i == j + 1) {
+                return new Pair<>(
+                        i-1, new PushNylonObjectFunction<>(new NylonCharacter(src.charAt(j)))
+                );
+            } else {
+                return new Pair<>(
+                        i-1, new PushNylonObjectFunction<>(new NylonString(src.substring(j, i)))
+                );
+            }
+        }
+        if (src.charAt(i) == '[') {
+            int depth = 1, j = i + 1;
+            for (i = j; i < src.length(); i++) {
+                if (src.charAt(i) == '[')
+                    depth++;
+                if (src.charAt(i) == ']')
+                    depth--;
+                if (depth == 0)
+                    break;
+            }
+            return new Pair<>(
+                    i-1, new NylonSrcFunction(runtime, src.substring(j, i), -1)
+            );
+        }
+        return new Pair<>(
+                i, runtime.getFunctionDictionary().get(runtime, src.charAt(i))
+        );
     }
 
 }
